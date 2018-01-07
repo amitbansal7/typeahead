fe8c5a566db34ec1b900b1df64098d760eb1be64
Amit Bansal <007amitbansal@gmail.com>
2018-01-08 01:39:37 +0530

    TODO: aggregation - suggestions

--- src/main/java/com/amitbansal7/typeahead/controller/NodeController.java
+++ src/main/java/com/amitbansal7/typeahead/controller/NodeController.java
@@ -1,8 +1,10 @@
 package com.amitbansal7.typeahead.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.amitbansal7.typeahead.model.Node;
 import com.amitbansal7.typeahead.repository.NodeRepository;
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+
 
 @Controller
 public class NodeController {
@@ -21,22 +26,102 @@ public class NodeController {
 	@Autowired
 	private NodeRepository nodeRepository;
 	
+	private final int MAXCOUNT = 10;
+	
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
+	private final String COLLECTION ="TrieNode";
+	
 	@RequestMapping(value="/", method=RequestMethod.POST)
-	public String inisert(@RequestParam("qry") String word) {
-		
+	public String insert(@RequestParam("qry") String word) {	
 		for(int i=1;i<=word.length();i++) {
 			String tempPrefix = word.substring(0, i);
 			
 			if(nodeRepository.existsByPrefix(tempPrefix) == false) {
 				nodeRepository.save(new Node(tempPrefix));
 			}
+			Criteria criteria = new Criteria().andOperator(Criteria.where("prefix").is(tempPrefix),Criteria.where("suggestions.word").is(word));
+			if(mongoTemplate.exists(new Query(criteria), Node.class, COLLECTION) == false) {		
+				BasicDBList updates = new BasicDBList();
+				BasicDBObject jsonSug = new BasicDBObject("word",word);
+				jsonSug.append("freq", 1);
+				BasicDBObject sugg = new BasicDBObject("suggestions", jsonSug);
+				BasicDBObject u = new BasicDBObject("$addToSet", sugg);
+				BasicDBObject q = new BasicDBObject("prefix", tempPrefix);
+				BasicDBObject oneQuery = new BasicDBObject();
+				oneQuery.append("q", q);
+				oneQuery.append("u", u);
+				updates.add(oneQuery);
+				BasicDBObject command = new BasicDBObject("update","TrieNode");
+				command.append("updates", updates);
+				mongoTemplate.executeCommand(command);
+			}else {
+				
+				BasicDBList updates = new BasicDBList();
+				BasicDBObject u = new BasicDBObject("$inc", new BasicDBObject("suggestions.$.freq",1));
+				BasicDBObject q  = new BasicDBObject();
+				q.append("prefix", tempPrefix);
+				q.append("suggestions.word", word);
+				BasicDBObject oneQuery = new BasicDBObject();
+				oneQuery.append("q", q);
+				oneQuery.append("u", u);
+				updates.add(oneQuery);
+				BasicDBObject command = new BasicDBObject("update", COLLECTION);
+				command.append("updates", updates);
+				mongoTemplate.executeCommand(command);
+			}
 			
-//			if(nodeRepository.existsByWord(tempPrefix, word) == false) {
-//				nodeRepository.updateByPrefix(tempPrefix, word);
-//			}
-//			else {
-//				continue;
-//			}
+			//sorting
+			BasicDBList updates = new BasicDBList();
+			BasicDBObject q  = new BasicDBObject("prefix",tempPrefix);
+			BasicDBObject oneQuery = new BasicDBObject();
+			oneQuery.append("q", q);
+			BasicDBObject sortSuggestions = new BasicDBObject("$each", new BasicDBList());
+			sortSuggestions.append("$sort", new BasicDBObject("freq",-1));
+			BasicDBObject u = new BasicDBObject("$push", new BasicDBObject("suggestions",sortSuggestions));
+			oneQuery.append("u", u);
+			updates.add(oneQuery);
+			BasicDBObject command = new BasicDBObject("update", COLLECTION);
+			command.append("updates", updates);
+			mongoTemplate.executeCommand(command);
+			
+			//find count
+//			BasicDBObject qA  = new BasicDBObject("$match", new BasicDBObject("prefix",tempPrefix));
+//			BasicDBObject commandA = new BasicDBObject("count", "TrieNode.suggestions");
+//			
+//			commandA.append("query", qA);
+//			BasicDBObject query = new BasicDBObject("query")
+//			MatchOperation res = mongoTemplate.executeCommand(commandA);
+			
+//			AggregationResults<DBObject> result = mongoTemplate.aggregate(
+//					Aggregation.match(new Criteria().where("prefix").is(tempPrefix)), DBObject.class);
+//			
+//			Criteria SCriteria = Criteria.where("prefix").is("prefix");
+//			MatchOperation matchOpr = Aggregation.match(SCriteria);
+//			ProjectionOperation projOp = Aggregation.project("suggestions");
+//			AggregationResults<DBObject> res = mongoTemplate.aggregate(Aggregation.newAggregation(
+//					matchOpr,
+//					projOp
+//			    ), Node.class, DBObject.class);
+//			
+//			System.out.println(res.toString());
+			
+			int count = 0;
+			//delete last if count is more than MAXCOUNT.
+			if(count > MAXCOUNT) {
+				BasicDBList updatesC = new BasicDBList();
+				BasicDBObject qC  = new BasicDBObject("prefix",tempPrefix);
+				BasicDBObject uC = new BasicDBObject("$pop", new BasicDBObject("suggestions",1));
+				BasicDBObject oneQueryC = new BasicDBObject();
+				oneQueryC.append("q", qC);
+				oneQueryC.append("u", uC);
+				updatesC.add(oneQueryC);
+				BasicDBObject commandC = new BasicDBObject("update", COLLECTION);
+				commandC.append("updates", updatesC);
+				mongoTemplate.executeCommand(commandC);
+			}
+			
 		}
 		return "home";
 	}
@@ -50,48 +135,4 @@ public class NodeController {
 	public void suggest(@PathVariable String word, Model model) {
 		 model.addAttribute("suggestions",nodeRepository.findByPrefix(word).getSuggestions());
 	}
-//	
-//	public List<String> suggest(String word) {
-//		Node crawl = root;
-//		List<String> res = new ArrayList<>();
-//		for (char c : word.toCharArray()) {
-//			if (crawl.children[c - firstChar] == null)
-//				return res;
-//
-//			crawl = crawl.children[c - firstChar];
-//		}
-//
-//		return crawl.getSuggestions();
-//	}
-//
-//	//Inserts a query in Trie
-//	public void insert(String word) {
-//		Node crawl = this.root;
-//		char firstChar = ' ';
-//
-//		for (char c : word.toCharArray()) {
-//			if (crawl.children[c - firstChar] == null)
-//				crawl.children[c - firstChar] = new Node();
-//
-//			crawl = crawl.children[c - firstChar];
-//
-//			crawl.addWord(word);
-//		}
-//	}
-	
-//	@RequestMapping(value="/",method=RequestMethod.GET )
-//	public Node getOne() {
-//		nodeRepository.save(new Node());
-//		Node n =  nodeRepository.findAll().get(0);
-//		String word = "abc";
-//		for (char c : word.toCharArray()) {
-//			if (n.children[c - 'a'] == null)
-//				n.children[c - 'a'] = new Node();
-//
-//			n = n.children[c - 'a'];
-//
-//			n.addWord(word);
-//		}
-//		return nodeRepository.findAll().get(0);
-//	}
 }
--- src/main/java/com/amitbansal7/typeahead/repository/NodeRepository.java
+++ src/main/java/com/amitbansal7/typeahead/repository/NodeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.amitbansal7.typeahead.model.Node;
+import com.amitbansal7.typeahead.model.Suggestion;
 
 @Repository
 public interface NodeRepository extends MongoRepository<Node, String>{
@@ -18,8 +19,8 @@ public interface NodeRepository extends MongoRepository<Node, String>{
 	
 	public Node findByPrefix(String prefix); 
 	
-	@Query(value="{\"prefix\":?0},{$addToSet:{suggestions:{\"word\":?1,\"freq\":1\"}}}")
-	public void updateByPrefix(String prefix, String word);
+	@Query(value="{\"prefix\":?0},{$addToSet:{suggestions:{?1}}}")
+	public void updateByPrefix(String prefix, Suggestion suggestion);
 	
 //	@Query(value="{\"prefix\":?0,\"suggestions.word\":?1},{$inc:{\"suggestions.$.freq\":1}}")
 //	public void updateByPrefix(String prefix, String word);
